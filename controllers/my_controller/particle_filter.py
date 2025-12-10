d16a44236764f85b5208f74131638d8f604a7d44
ConnrUni <cxb1079@student.bham.ac.uk>
2025-12-10 23:09:44 +0000

    Improved Particle filter

--- controllers/my_controller/particle_filter.py
+++ controllers/my_controller/particle_filter.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 import random as r
+from copy import deepcopy
 
 def lidar_point_to_grid(px, py, ROBOT_THETA, ROBOT_X, ROBOT_Y):
     """
@@ -16,9 +17,18 @@ def lidar_point_to_grid(px, py, ROBOT_THETA, ROBOT_X, ROBOT_Y):
     abs_y = ROBOT_Y + rotated_y
 
     # Convert to grid coordinates
-    grid_x = int(round(abs_x / 0.25))
-    grid_y = int(round(abs_y / 0.25))
+    grid_x = int(round(abs_x / 0.05))
+    grid_y = int(round(abs_y / 0.05))
     
+    grid_x += 50
+    grid_y += 50
+
+    grid_x = max(grid_x,0)
+    grid_x = min(grid_x, 99)
+
+    grid_y = max(grid_y,0)
+    grid_y = min(grid_y,99)
+
     return grid_x, grid_y
 
 def Particle_filter(particles, movement, sensor_read, map) :
@@ -34,20 +44,24 @@ def Particle_filter(particles, movement, sensor_read, map) :
     #                                 Mean, Standard Distribution, Size of array
     move_random = movement[0] + (np.random.normal(0.0, 1.0, len(particles))  * movement[0] / 20) #20 is how much randomness affects the movements 
     rotate_random = movement[1] + (np.random.normal(0.0, 1.0, len(particles)) * movement[1] / 20)#20 Can be changed
-
+    
     #Update positions with moving
     for i in range(len(particles)) :
-        #x-coord
-        particles[i][0] += move_random[i] * math.cos(rotate_random[i])
-        #y-coord
-        particles[i][1] += move_random[i] * math.sin(rotate_random[i])
+
         #Rotation
         particles[i][2] += rotate_random[i]
+
+        #x-coord
+        particles[i][0] += move_random[i] * math.cos(particles[i][2])
+        #y-coord
+        particles[i][1] += move_random[i] * math.sin(particles[i][2])
+
+        
     
     #Weight each point based off of sensor data
     weighted_particles = []
     for point in particles :
-        sum_of_squares = 0
+        weight = 1
         #Check each direction compared to the map
         for i in range(len(sensor_read)) :
             
@@ -56,67 +70,43 @@ def Particle_filter(particles, movement, sensor_read, map) :
             if math.isnan(sensor_read[i].y) or math.isnan(sensor_read[i].y) :
                  continue
             
-            hit1 = False
             position = [point[0],point[1]] #[x,y]
             
             #Converts sensor point to a point on the grid based off of the particle's position and facing
             sensor = lidar_point_to_grid(sensor_read[i].x, sensor_read[i].y, point[2], position[0], position[1])
             
+            if map[-sensor[1]][sensor[0]] == 1 :
+                 weight *= 1.2
+            else :
+                weight *= 0.5
 
-            #If not hit and position inside the map 
-            while (not(hit1) and 0 <= position[0] <= len(map[0]) and 0 <= position[1] <= len(map)) :
-                   
-                   if (map[round(position[0])][round(position[1])] == 1) :
-                        hit1 = True
-                        break
-                   
-                   else :
-                        #Move to the next position
-                        #Shortest distance to make it to the next coord in x or y
-                        
-                         if (sensor[0] > 0) :
-                              x = ((round(position[0]) - 0.5) - position[0]) / sensor[0]
-                         else :
-                              x = ((round(position[0]) + 0.5) - position[0]) / sensor[0]
+        #new weight
+        point[3] = weight
+        weighted_particles.append(weight)
 
-                         if (sensor[1] > 0) :
-                              y = ((round(position[1]) - 0.5) - position[1]) / sensor[1]
-                         else :
-                              y = ((round(position[1]) + 0.5) - position[1]) / sensor[1]
-
-                         if x < y :
-                              position[0] += x * sensor[0]
-                              position[1] += x * sensor[1]
-                         else :
-                              position[0] += y * sensor[0]
-                              position[1] += y * sensor[1]
-
-                                  
-            
-            #If hit wall, find distance and find difference to sensor read
-            if hit1 :
-                 distance = [abs(sensor_read[i].x - position[0]), abs(sensor_read[i].y - position[1])] 
-
-                 sum_of_squares +=  math.sqrt(sum(x*x for x in distance))
-
-        point[3] = 1 - sum_of_squares
-        #1 / sum_of_squares is to give smaller differences more weight
-        if (sum_of_squares != 0) :
-              weighted_particles.append(1 / sum_of_squares)
-        else :
-              weighted_particles.append(0)
-
-
+    
     #Now sample new points based off of these weighted points
     weight_sum = sum(weighted_particles)
-    newParticles = []
-    for i in range(len(particles)) :
-         pick = r.random() * weight_sum
 
+    #Turn all weights into percentages
+    for i in range(len(particles)) :
+         particles[i][3] /= weight_sum
+         weighted_particles[i] /= weight_sum
+
+
+    newParticles = []
+
+
+
+    for i in range(len(particles)) :
+         pick = r.random()
          for j in range(len(weighted_particles)) :
               pick -= weighted_particles[j] 
               if pick <= 0 :
-                   newParticles.append(particles[j])
+                   
+                   newParticles.append(deepcopy(particles[j]))
+                   break
+              
 
 
     particles = newParticles
